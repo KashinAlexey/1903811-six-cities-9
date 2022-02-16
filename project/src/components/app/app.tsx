dc0b738e5f685174e2565faff2e7666de72d5624
Alexey <apple230379@gmail.com>
2022-02-16 16:22:10 +0300

    Replace Route with PrivateRoute

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,11 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import {AppRoute} from '../../const';
+import {AuthorizationStatus} from '../../const';
 import MainScreen from '../main-screen/main-screen';
 import FavoritesScreen from '../favorites-screen/favorites-screen';
 import LoginScreen from '../login-screen/login-screen';
 import PropertyScreen from '../property-screen/property-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   cardsCount: number;
@@ -19,7 +21,13 @@ function App({cardsCount}: AppScreenProps): JSX.Element {
         />
         <Route
           path={AppRoute.Favorites}
-          element={<FavoritesScreen />}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Login}
