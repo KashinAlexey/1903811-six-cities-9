8d380c6802486ded93f623937aa492581eec289a
Alexey <apple230379@gmail.com>
2022-02-16 15:55:25 +0300

    Add routing

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,9 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainScreen from '../main-screen/main-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import LoginScreen from '../login-screen/login-screen';
+import PropertyScreen from '../property-screen/property-screen';
 
 type AppScreenProps = {
   cardsCount: number;
@@ -6,7 +11,26 @@ type AppScreenProps = {
 
 function App({cardsCount}: AppScreenProps): JSX.Element {
   return (
-    <MainScreen cardsCount={cardsCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainScreen cardsCount={cardsCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesScreen />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path={AppRoute.Property}
+          element={<PropertyScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/const.ts
@@ -0,0 +1,12 @@
+export enum AppRoute {
+  Root = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Property = '/property',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
