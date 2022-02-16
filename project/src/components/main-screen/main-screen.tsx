161e5a705707cde8dc91ad394deaa7578a44e60d
Alexey <apple230379@gmail.com>
2022-02-16 16:11:51 +0300

    Updates the link for the logo

--- project/src/components/main-screen/main-screen.tsx
+++ project/src/components/main-screen/main-screen.tsx
@@ -1,4 +1,5 @@
 import CitiesPlacesCard from '../cities-places-card/cities-places-card';
+import Logo from '../logo/logo';
 
 type MainScreenProps = {
   cardsCount: number;
@@ -12,11 +13,7 @@ function MainScreen({cardsCount}: MainScreenProps): JSX.Element {
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              <a href="#section" className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
+            <Logo />
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
